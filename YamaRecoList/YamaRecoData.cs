56cf768628f97602d67986bef572a8a2fed4e07f
Katsushige Yoshida <k-yosida@g06.itscom.net>
2022-08-06 19:47:57 +0900

    バグ修正

--- YamaRecoList/YamaRecoData.cs
+++ YamaRecoList/YamaRecoData.cs
@@ -229,10 +229,12 @@ namespace MapApp
         public List<string[]> extractListdata(List<string[]> listData)
         {
             List<string[]> yamaListdata = new List<string[]>();
-            for (int i = 0; i < listData.Count; i++) {
-                int n = mDataList.FindIndex(p => p[titleNo("URL")].CompareTo(listData[i][0]) == 0);
-                if (0 <= n)
-                    yamaListdata.Add(mDataList[n]);
+            if (0 < mDataList.Count) {
+                for (int i = 0; i < listData.Count; i++) {
+                    int n = mDataList.FindIndex(p => p[titleNo("URL")].CompareTo(listData[i][0]) == 0);
+                    if (0 <= n)
+                        yamaListdata.Add(mDataList[n]);
+                }
             }
 
             return yamaListdata;
